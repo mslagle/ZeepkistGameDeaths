c537a990f77b5d8bd5001ad88c6bb0ffe2e9143c
Matt Slagle <Matt.Slagle@onbase.com>
2023-08-25 11:00:00 -0400

    Feature:  Initial version of the mod

---
+++ Zeepkist.Deaths/Deaths/DeathResourceManager.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Zeepkist.Deaths.Deaths
+{
+    internal static class DeathResourceManager
+    {
+        static DeathsEnum LastDeathTexture = DeathsEnum.Disabled;
+        static DeathsEnum LastDeathAudio = DeathsEnum.Disabled;
+
+        static Texture2D texture = null;
+        static AudioClip audioclip = null;
+
+        public static Texture2D GetDeathTexture(DeathsEnum deathsEnum)
+        {
+            if (deathsEnum == LastDeathTexture && texture != null)
+            {
+                return texture;
+            }
+
+            LastDeathTexture = deathsEnum;
+            if (deathsEnum == DeathsEnum.DarkSouls)
+            {
+                texture = LoadTexture("darksouls.png");
+            }
+            if (deathsEnum == DeathsEnum.GTA)
+            {
+                texture = LoadTexture("gta.png");
+            }
+            if (deathsEnum == DeathsEnum.MortalKombat)
+            {
+                texture = LoadTexture("mk.png");
+            }
+
+            return texture;
+        }
+
+        public static async Task<AudioClip> GetDeathAudio(DeathsEnum deathsEnum)
+        {
+            if (deathsEnum == LastDeathAudio && audioclip != null)
+            {
+                return audioclip;
+            }
+
+            LastDeathAudio = deathsEnum;
+            if (deathsEnum == DeathsEnum.DarkSouls)
+            {
+                audioclip = await GetAudioClip("darksouls.mp3");
+            }
+            if (deathsEnum == DeathsEnum.GTA)
+            {
+                audioclip = await GetAudioClip("gta.mp3");
+            }
+            if (deathsEnum == DeathsEnum.MortalKombat)
+            {
+                audioclip = await GetAudioClip("mk.mp3");
+            }
+
+            return audioclip;
+        }
+
+        private static Texture2D LoadTexture(string path)
+        {
+            Debug.Log($"Loading a texture from path ${path}");
+
+            string dllFile = System.Reflection.Assembly.GetAssembly(typeof(DeathResourceManager)).Location;
+            string dllDirectory = Path.GetDirectoryName(dllFile);
+            string imagePath = Path.Combine(dllDirectory, path);
+
+            byte[] imageBytes = File.ReadAllBytes(imagePath);
+            Texture2D texture = new Texture2D(1, 1);
+            texture.LoadImage(imageBytes);
+
+            return texture;
+        }
+
+        public static async Task<AudioClip> GetAudioClip(string path)
+        {
+            Debug.Log($"Creating an audio clip from path ${path}");
+
+            string dllFile = System.Reflection.Assembly.GetAssembly(typeof(DeathResourceManager)).Location;
+            string dllDirectory = Path.GetDirectoryName(dllFile);
+            string audioPath = Path.Combine(dllDirectory, path);
+
+            AudioClip clip = null;
+            using (UnityWebRequest uwr = UnityWebRequestMultimedia.GetAudioClip(audioPath, AudioType.MPEG))
+            {
+                uwr.SendWebRequest();
+
+                // wrap tasks in try/catch, otherwise it'll fail silently
+                try
+                {
+                    while (!uwr.isDone) await Task.Delay(5);
+
+                    if (uwr.isNetworkError || uwr.isHttpError) Debug.Log($"{uwr.error}");
+                    else
+                    {
+                        clip = DownloadHandlerAudioClip.GetContent(uwr);
+                    }
+                }
+                catch (Exception err)
+                {
+                    Debug.Log($"{err.Message}, {err.StackTrace}");
+                }
+            }
+
+            return clip;
+        }
+    }
+}
---
+++ Zeepkist.Deaths/Plugin.cs
@@ -0,0 +1,56 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System;
+using UnityEngine;
+using Zeepkist.Deaths.Deaths;
+
+namespace Zeepkist.Deaths
+{
+
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("ZeepSDK")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private Harmony harmony;
+
+        public static ConfigEntry<string> Death { get; private set; }
+
+        private void Awake()
+        {
+            harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+            harmony.PatchAll();
+
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+            Plugin.Death = this.Config.Bind<string>("Mod", "Death", DeathsEnum.DarkSouls.ToString(), 
+                new ConfigDescription("Death scene to play when dead",
+                new AcceptableValueList<string>(Enum.GetNames(typeof(DeathsEnum)))));
+            Plugin.Death.SettingChanged += Death_SettingChanged;
+
+            DeathManager.Initialize();
+            DeathManager.deaths = Enum.Parse<DeathsEnum>(Plugin.Death.Value);
+        }
+
+        private void Death_SettingChanged(object sender, EventArgs e)
+        {
+            Debug.Log($"Mod status changed.  New status = ${Death.Value}");
+            DeathManager.deaths = Enum.Parse<DeathsEnum>(Plugin.Death.Value);
+        }
+
+        public void OnGUI()
+        {
+            if (Enum.Parse<DeathsEnum>(Plugin.Death.Value) != DeathsEnum.Disabled)
+            {
+                DeathManager.OnGui();
+            }
+        }
+
+        public void OnDestroy()
+        {
+            harmony?.UnpatchSelf();
+            harmony = null;
+        }
+    }
+}
